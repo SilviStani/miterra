130d80f484901ced0f2ed31f0de2623d4178ab21
SilviStani <silvina.stani@gmail.com>
2025-05-16 13:53:21 -0300

    adding new features

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
+  allowedDevOrigins: ['http://192.168.0.104:3000'],
+}
+
+module.exports = nextConfig
